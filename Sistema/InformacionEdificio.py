a5c8b98fbda2be2f52e0ee137afae8fe2822cd6d
PaolaCanelas <canelaspaola0@gmail.com>
2024-03-06 17:50:25 -0400

    control sistemas update

--- Sistema/InformacionEdificio.py
+++ Sistema/InformacionEdificio.py
@@ -6,6 +6,7 @@ class Edificio:
         self.pisos = pisos
         self.ascensores = ascensores
 
+
 class Piso:
     def __init__(self, numero_piso, ascensor):
         self.numero_piso = numero_piso
@@ -27,20 +28,25 @@ class Piso:
         self.ascensor.abierto = False
         self.boton_abajo = False
 
+
 class Ascensor:
-    def __init__(self, botones_de_piso):
+    def __init__(self, botones_de_piso, ascensorA, ascensorB):
         self.botones_de_piso = botones_de_piso
         self.piso = 0
         self.abierto = False
+        self.ascensorA = ascensorA
+        self.ascensorB = ascensorB
 
     def ir_al_piso(self, numero_de_piso):
         botones = list(filter(lambda button: button.numero_de_piso == numero_de_piso, self.botones_de_piso))
+        if self.ascensorA in botones:
+            boton_presionado = self.ascensorA
+            print(f'Ir al Piso: {boton_presionado.numero_de_piso}')
+        elif self.ascensorB in botones:
+            boton_presionado = self.ascensorB
+            print(f'Ir al Piso: {boton_presionado.numero_de_piso}')
         print('----------------')
-        boton_presionado = botones[0]
-        print(boton_presionado.numero_de_piso)
-        print(f'--Yendo al Piso: {boton_presionado.numero_de_piso}')
-        print('----------------')
-        print(f'--Bajando al piso: {boton_presionado.numero_de_piso}')
+
 
 class BotonDePiso:
     def __init__(self, numero_de_piso):
@@ -48,31 +54,38 @@ class BotonDePiso:
         self.encendido = False
 
 
-
 boton_piso0 = BotonDePiso(0)
 boton_piso1 = BotonDePiso(1)
 boton_piso2 = BotonDePiso(2)
 boton_piso3 = BotonDePiso(3)
-botones_de_pisos = [boton_piso0, boton_piso1, boton_piso2, boton_piso3]
+boton_piso4 = BotonDePiso(4)
+boton_piso5 = BotonDePiso(5)
 
+
+botones_de_pisos = [boton_piso0, boton_piso1, boton_piso2, boton_piso3, boton_piso4, boton_piso5]
 ascensorA = Ascensor(botones_de_pisos)
+ascensorB = Ascensor(botones_de_pisos)
+
 piso0 = Piso(0, ascensorA)
+piso0 = Piso(0, ascensorB)
 piso1 = Piso(1, ascensorA)
+piso1 = Piso(1, ascensorB)
 piso2 = Piso(2, ascensorA)
+piso2 = Piso(2, ascensorB)
 piso3 = Piso(3, ascensorA)
+piso3 = Piso(3, ascensorB)
+piso4 = Piso(4, ascensorA)
+piso4 = Piso(4, ascensorB)
+piso5 = Piso(5, ascensorA)
+piso5 = Piso(5, ascensorB)
+pisos = [piso0, piso1, piso2, piso3, piso4, piso5]
 
+edificio_carilo = Edificio(pisos, [ascensorA, ascensorB])
 
-
-pisos = [piso0, piso1, piso2, piso3]
-
-
-
-ascensores = [ascensorA]
-
-edificio_carilo = Edificio(pisos, ascensorA)
 
 piso0.ir_piso_arriba()
 ascensorA.ir_al_piso(3)
+piso0.ir_piso_arriba()
+ascensorB.ir_al_piso(3)
 piso2.ir_piso_abajo()
 ascensorA.ir_al_piso(1)
-# edificio_torre = Configuracion(15, 2)
